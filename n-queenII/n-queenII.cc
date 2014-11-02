4981b830260964ee278d6cf3cbc434ebad35e282
cheerayhuang <cheeray.huang@gmail.com>
2014-11-02 08:39:09 +0800

    [add] N queen II 	add:   n-queenII/n-queenII.cc

---
+++ n-queenII/n-queenII.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std; 
+
+class Solution {
+private:
+    vector<int> board;
+    vector<bool> column;
+    int total;
+
+    bool CheckDiagonal(int x, int y) {
+        for (auto i = 0; i < x; ++i) {
+            if (x-i == abs(y - board[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    int PlaceChess(int step, int n) {
+        if (step == n) {
+            ++total; 
+            return 0; 
+        }
+
+        for (auto i = 0; i < n; ++i) {
+           if (column[i] && CheckDiagonal(step, i)) {
+               column[i] = false;  
+               board.push_back(i); 
+               PlaceChess(step+1, n);
+               board.pop_back();
+               column[i] = true;
+           }
+        }
+    }
+
+public:
+    int SolveNQueen(int n) {
+        board.clear(); 
+        column.assign(n, true);
+        total = 0;
+        PlaceChess(0, n);
+
+        return total;
+    }
+};
+
+int main() {
+    Solution s;
+
+    cout << s.SolveNQueen(8) << endl;
+    cout << s.SolveNQueen(1) << endl;
+    cout << s.SolveNQueen(4) << endl;
+
+    return 0;
+}
